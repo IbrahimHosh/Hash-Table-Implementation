a04e556a42647dc77b732956d049e3df6d393338
IbrahimHosh <97615900+IbrahimHosh@users.noreply.github.com>
2022-01-14 11:09:13 -0600

    Update TextScan.java

--- IbrahimHosh_Project1/IbrahimHosh_Project1/TextScan.java
+++ IbrahimHosh_Project1/IbrahimHosh_Project1/TextScan.java
@@ -1,10 +1,3 @@
-// TextScan.java
-// Opens text file supplied on the command line
-// Usage:  java TextScan filename
-// Counts all tokens found (a token is anything delimited by a space character)
-// Displays each token found to the screen
-// Code may be used in part for Project 5 with proper citing. 
-
 import java.util.Scanner;
 import java.io.*;
 
@@ -44,6 +37,6 @@ public class TextScan {
     }
 
     public static void main(String[] args) {
-    }  // main
+    }  
 
-}  // TextScan
+}  
